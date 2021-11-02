873069ec8042f44a2998fd1ff7300431c6284373
Ahmet <scienceworker34@gmail.com>
2021-11-02 12:27:20 +0300

    activate sqlite3

--- main/settings.py
+++ main/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME': 'inclass',  #db propertiesden
-      'USER':'postgres',     #db propertiesden
-      'PASSWORD':'*****',  # postgresql password
-      'HOST':'localhost',   
-      'PORT':'5432',            
-   }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES={
+#    'default':{
+#       'ENGINE':'django.db.backends.postgresql_psycopg2',
+#       'NAME': 'inclass',  #db propertiesden
+#       'USER':'postgres',     #db propertiesden
+#       'PASSWORD':'*****',  # postgresql password
+#       'HOST':'localhost',   
+#       'PORT':'5432',            
+#    }
+# }
 
 
 # Password validation
